747fa08c005c92896de1aacc7c3a503cd744acc6
Иван <cvb@sdf>
2024-09-04 23:07:08 +0300

    KS-7-2-authorization-by-sms Создал модуль для авторизации по СМС

--- src/main/kotlin/lesson_7/task_2.kt
+++ src/main/kotlin/lesson_7/task_2.kt
@@ -1,5 +1,15 @@
 package org.example.lesson_7
 
 fun main(){
-
+    val digitRange = 1000..9999
+    while (true){
+        val smsCode = digitRange.random().toString()
+        println("Ваш код авторизации:$smsCode")
+        println("Введите код полученный в СМС сообщении")
+        val enteredCode = readln()
+        if (smsCode==enteredCode){
+            println("Добро пожаловать")
+            return
+        }
+    }
 }
