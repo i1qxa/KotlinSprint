024804daffcdb816918bed7e9efa1a34c505c2fd
Иван <cvb@sdf>
2024-09-04 23:04:41 +0300

    KS-7-1-pass-generator Изменил диапазон генерации случайного числа с 0..8 на 0..9

--- src/main/kotlin/tmp/task_2.kt
+++ src/main/kotlin/tmp/task_2.kt
@@ -1,22 +1,15 @@
 package org.example.tmp
 
 fun main(){
-    var isSecondsSuccess = false
-    var secondsAsInt = 0
-    while (!isSecondsSuccess){
-        println("Введите количество секунд которые необходимо засечь")
-        val secondsAsString = readln()
-        try {
-            secondsAsInt = secondsAsString.toInt()
-            isSecondsSuccess=true
-        }catch (e:NumberFormatException){
-            println("Ошибка. Секунды нужно ввести в числовом формате")
+    val digitRange = 1000..9999
+    while (true){
+        val smsCode = digitRange.random().toString()
+        println("Ваш код авторизации:$smsCode")
+        println("Введите код полученный в СМС сообщении")
+        val enteredCode = readln()
+        if (smsCode==enteredCode){
+            println("Добро пожаловать")
+            return
         }
     }
-    var counter = 0
-    do {
-        Thread.sleep(1000)
-        counter++
-    }while (counter<secondsAsInt)
-    println("Прошло $secondsAsInt секунд")
 }
