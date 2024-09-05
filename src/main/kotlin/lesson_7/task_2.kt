4d0b7e1a4c08c62d90dafb16240e809996e5260c
Иван <cvb@sdf>
2024-09-05 11:34:04 +0300

    KS-7-2-authorization-by-sms Поправил форматирование

--- src/main/kotlin/lesson_7/task_2.kt
+++ src/main/kotlin/lesson_7/task_2.kt
@@ -1,13 +1,13 @@
 package org.example.lesson_7
 
-fun main(){
+fun main() {
     val digitRange = 1000..9999
-    while (true){
+    while (true) {
         val smsCode = digitRange.random().toString()
         println("Ваш код авторизации:$smsCode")
         println("Введите код полученный в СМС сообщении")
         val enteredCode = readln()
-        if (smsCode==enteredCode){
+        if (smsCode==enteredCode) {
             println("Добро пожаловать")
             return
         }
