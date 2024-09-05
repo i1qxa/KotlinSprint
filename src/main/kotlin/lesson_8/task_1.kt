5d4472ce719618a5f78b6510146eff7d79e702ab
Иван <cvb@sdf>
2024-09-05 22:45:29 +0300

    KS-8-1-watch-counter Сделал программу для подсчета количества просмотров за неделю

--- src/main/kotlin/lesson_8/task_1.kt
+++ src/main/kotlin/lesson_8/task_1.kt
@@ -1,5 +1,10 @@
 package org.example.lesson_8
 
-fun main(){
-
+fun main() {
+    val watchArray = intArrayOf(25, 75, 15, 65, 95, 5, 125)
+    var totalWatchAmount = 0
+    watchArray.map {
+        totalWatchAmount += it
+    }
+    println("Всего просмотров за неделю: $totalWatchAmount")
 }
