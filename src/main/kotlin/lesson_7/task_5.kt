2895b3175f72de7e9ae29f29329059a5c6e68d92
Иван <cvb@sdf>
2024-09-05 11:47:45 +0300

    KS-7-5-hard-pass-generator Упростил генерацию пароля

--- src/main/kotlin/lesson_7/task_5.kt
+++ src/main/kotlin/lesson_7/task_5.kt
@@ -16,29 +16,12 @@ fun main() {
     val alphabetUpper = 'A'..'Z'
     val digitInterval = 0..9
     val validCharacters = alphabet + alphabetUpper + digitInterval
-    var isCharExist = false
-    var isCharUpperExist = false
-    var isDigitExist = false
     val password = StringBuilder()
+    password.append(alphabet.random())
+    password.append(alphabetUpper.random())
+    password.append(digitInterval.random())
+    passLength-=3
     while (passLength > 0) {
-        if (!isCharExist) {
-            password.append(alphabet.random())
-            isCharExist = true
-            passLength--
-            continue
-        }
-        if (!isCharUpperExist) {
-            password.append(alphabetUpper.random())
-            isCharUpperExist = true
-            passLength--
-            continue
-        }
-        if (!isDigitExist) {
-            password.append(digitInterval.random())
-            isDigitExist = true
-            passLength--
-            continue
-        }
         password.append(validCharacters.random())
         passLength--
     }
