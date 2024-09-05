db7f961b8d24368e79eb6ec6a68b0277c09af7af
Иван <cvb@sdf>
2024-09-05 23:39:43 +0300

    KS-8-4-ingredient-change Сделал программу для замены ингредиента в рецепте.

---
+++ src/main/kotlin/lesson_8/task_4.kt
@@ -0,0 +1,32 @@
+package org.example.lesson_8
+
+fun main() {
+    val ingredientArray = arrayOf(
+        "яйца",
+        "масло",
+        "бекон",
+        "зелень",
+        "соль",
+    )
+    println("Список ингредиентов: ${ingredientArray.toFormattedString()}")
+    println("Какой ингредиент вы хотите заменить?")
+    val targetIngredient = readln()
+    val targetIngredientIndex = ingredientArray.indexOf(targetIngredient)
+    if (targetIngredientIndex >= 0) {
+        println("На какой ингредиент хотите заменить $targetIngredient?")
+        val newIngredient = readln()
+        ingredientArray[targetIngredientIndex] = newIngredient
+        println("Готово! Вы сохранили следующий список: ${ingredientArray.toFormattedString()}")
+    } else {
+        println("Такого ингредиента нет в списке")
+    }
+}
+
+fun Array<String>.toFormattedString():String {
+    val arrayAsStringBuilder = StringBuilder()
+    this.map {
+        arrayAsStringBuilder.append(it)
+        arrayAsStringBuilder.append(", ")
+    }
+    return (arrayAsStringBuilder.dropLast(2).toString())
+}
