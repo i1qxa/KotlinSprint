72576c23e40207cab64f5463c373d88f9521c11b
Иван <cvb@sdf>
2024-09-11 01:16:49 +0300

    KS-8-5-create-recipe Сделал создание массива через лямбду. И распечатку ингридиентов через joinToString

--- src/main/kotlin/lesson_8/task_5.kt
+++ src/main/kotlin/lesson_8/task_5.kt
@@ -2,17 +2,6 @@ package org.example.lesson_8
 
 fun main(){
     println("Укажите количество ингредиентов в рецепте")
-    val ingredientAmount = readln().toInt()
-    val ingredientArray = Array(ingredientAmount) { "" }
-    var index = 0
-    while (index < ingredientAmount) {
-        ingredientArray[index] = readln()
-        index++
-    }
-    val recipe = StringBuilder()
-    ingredientArray.map {
-        recipe.append(it)
-        recipe.append(", ")
-    }
-    println("Список ингредиентов: ${recipe.dropLast(2)}")
+    val ingredientArray = Array(readln().toInt()) { readln() }
+    println("Список ингредиентов: ${ingredientArray.joinToString(separator = ", ")}")
 }
