cd741816bc051b24bd24824416afdac1184b5619
Иван <cvb@sdf>
2024-09-11 01:38:50 +0300

    KS-9-2-recipe-mutable Создал приложения для отображения списка ингредиентов с возможностью добавления нового ингредиента

---
+++ src/main/kotlin/lesson_9/task_2.kt
@@ -0,0 +1,18 @@
+package org.example.lesson_9
+
+import java.util.*
+
+fun main() {
+    val baseRecipeList = mutableListOf("Яйца", "Бекон", "Масло растительное")
+    println("В рецепте есть базовые ингредиенты: ${baseRecipeList.joinToString(separator = ", ")}")
+    println("Желаете добавить еще?")
+    val shouldAddNewRecipeItem = readln().lowercase(Locale.getDefault()) == "да"
+    if (shouldAddNewRecipeItem) {
+        println("Какой ингредиент вы хотите добавить?")
+        baseRecipeList.add(readln())
+        println("Теперь в рецепте есть следующие ингредиенты: ${baseRecipeList.joinToString(separator = ", ")}")
+    }
+    else {
+        return
+    }
+}
