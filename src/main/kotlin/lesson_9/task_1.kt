7eea83d83b91c3da48f243ed8892503b5eaca74a
Иван <cvb@sdf>
2024-09-12 22:27:20 +0300

    KS-9-1-recipe-list Заменил map на foreach

--- src/main/kotlin/lesson_9/task_1.kt
+++ src/main/kotlin/lesson_9/task_1.kt
@@ -3,7 +3,7 @@ package org.example.lesson_9
 fun main() {
     val recipeList = listOf("Капуста", "Картофель", "Мясо", "Свекла", "Зелень", "Специи")
     println("В рецепте есть следующие ингредиенты:")
-    recipeList.map { recipeItem ->
+    recipeList.forEach { recipeItem ->
         println(recipeItem)
     }
 }
