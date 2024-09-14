2ab89b76bc0fb6b949ba2016ba08900688245b4f
Иван <cvb@sdf>
2024-09-14 23:52:59 +0300

    KS-9-4-request-ingredient Почистил код

--- src/main/kotlin/lesson_9/task_4.kt
+++ src/main/kotlin/lesson_9/task_4.kt
@@ -1,6 +1,6 @@
 package org.example.lesson_9
 
-fun main(){
+fun main() {
     println("Введите пять ингредиентов разделенных , (запятой с пробелом)")
     val inputIngredients = readln().split(", ")
     val sortedIngredientList = inputIngredients.sorted()
