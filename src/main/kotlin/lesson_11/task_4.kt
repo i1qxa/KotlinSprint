66c56267e61d3f4b835ec41490932043d34b7948
Иван <cvb@sdf>
2024-09-15 22:41:07 +0300

    KS-11-4-recipe-entities Спроектировал классы по макету

---
+++ src/main/kotlin/lesson_11/task_4.kt
@@ -0,0 +1,25 @@
+package org.example.lesson_11
+
+class RecipeCategory(
+    val id: Int,
+    val title: String,
+    val logo: String,
+    val description: String,
+)
+
+class Ingredient(
+    val id: Int,
+    val name: String,
+    val amount: Double,
+    val unitOM: String,
+)
+
+class Recipe(
+    val id: Int,
+    val name: String,
+    val logo: String,
+    var inFavorite: Boolean = false,
+    val categoryId: Int,
+    val numberServings: Int,
+    val ingredients: List<Ingredient>,
+)
