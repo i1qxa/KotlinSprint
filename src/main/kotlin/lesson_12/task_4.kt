30c08504169f47ea130314cde4ad404d736578dc
Иван <cvb@sdf>
2024-09-18 00:21:36 +0300

    KS-12-4-weather-init Перенес вывод информации о погоде в блок init

--- src/main/kotlin/lesson_12/task_4.kt
+++ src/main/kotlin/lesson_12/task_4.kt
@@ -1,17 +1,23 @@
 package org.example.lesson_12
 
+const val DIFFERENT_BETWEEN_KELVIN_AND_CELSIUS = 273
+
 fun main() {
-    val weather = WeatherData2(11, 2, true)
+    val weather = WeatherData1(300, 289, true)
 }
 
-class WeatherData2(_dayTemp: Int, _nightTemp: Int, _isPrecipitation: Boolean) {
+class WeatherData1(_dayTempK: Int, _nightTempK: Int, _isPrecipitation: Boolean) {
 
-    val dayTemp = _dayTemp
-    val nightTemp = _nightTemp
+    val dayTempC = _dayTempK - DIFFERENT_BETWEEN_KELVIN_AND_CELSIUS
+    val nightTempC = _nightTempK - DIFFERENT_BETWEEN_KELVIN_AND_CELSIUS
     val isPrecipitation = _isPrecipitation
 
+    init {
+        printWeatherData()
+    }
+
     fun printWeatherData() {
-        println("Температура днём:$dayTemp. Температура ночью:$nightTemp. ${getPrecipitationAsString()}")
+        println("Температура днём:$dayTempC. Температура ночью:$nightTempC. ${getPrecipitationAsString()}")
     }
 
     private fun getPrecipitationAsString(): String = if (isPrecipitation) "Были осадки." else "Осадков не было."
