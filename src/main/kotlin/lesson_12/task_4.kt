967d18fe47b7e269b51f74e865c36589f33dcc07
Иван <cvb@sdf>
2024-09-18 00:10:14 +0300

    KS-12-4-weather-init 1

---
+++ src/main/kotlin/lesson_12/task_4.kt
@@ -0,0 +1,19 @@
+package org.example.lesson_12
+
+fun main() {
+    val weather = WeatherData2(11, 2, true)
+}
+
+class WeatherData2(_dayTemp: Int, _nightTemp: Int, _isPrecipitation: Boolean) {
+
+    val dayTemp = _dayTemp
+    val nightTemp = _nightTemp
+    val isPrecipitation = _isPrecipitation
+
+    fun printWeatherData() {
+        println("Температура днём:$dayTemp. Температура ночью:$nightTemp. ${getPrecipitationAsString()}")
+    }
+
+    private fun getPrecipitationAsString(): String = if (isPrecipitation) "Были осадки." else "Осадков не было."
+
+}
