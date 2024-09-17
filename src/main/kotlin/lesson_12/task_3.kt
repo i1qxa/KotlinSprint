407fb626d01c8242f514fab0c5c509411ee58ede
Иван <cvb@sdf>
2024-09-18 00:18:38 +0300

    KS-12-3-weather-3 Добавил перевод температуры из кельвинов в цельсии

--- src/main/kotlin/lesson_12/task_3.kt
+++ src/main/kotlin/lesson_12/task_3.kt
@@ -1,18 +1,20 @@
 package org.example.lesson_12
 
-fun main(){
-    val weather = WeatherData1(11,2,true)
+const val DIFFERENT_BETWEEN_KELVIN_AND_CELSIUS = 273
+
+fun main() {
+    val weather = WeatherData1(11, 2, true)
     weather.printWeatherData()
 }
 
-class WeatherData1(_dayTemp: Int, _nightTemp: Int, _isPrecipitation: Boolean) {
+class WeatherData1(_dayTempK: Int, _nightTempK: Int, _isPrecipitation: Boolean) {
 
-    val dayTemp = _dayTemp
-    val nightTemp = _nightTemp
+    val dayTempC = _dayTempK - DIFFERENT_BETWEEN_KELVIN_AND_CELSIUS
+    val nightTempC = _nightTempK - DIFFERENT_BETWEEN_KELVIN_AND_CELSIUS
     val isPrecipitation = _isPrecipitation
 
     fun printWeatherData() {
-        println("Температура днём:$dayTemp. Температура ночью:$nightTemp. ${getPrecipitationAsString()}")
+        println("Температура днём:$dayTempC. Температура ночью:$nightTempC. ${getPrecipitationAsString()}")
     }
 
     private fun getPrecipitationAsString(): String = if (isPrecipitation) "Были осадки." else "Осадков не было."
