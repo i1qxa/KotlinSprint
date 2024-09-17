71627a91e9fdecc50ae67ff341a6db2b51e23641
Иван <cvb@sdf>
2024-09-17 17:16:59 +0300

    KS-12-1-weather Создал класс для работы с данными о погоде с пустым конструктором

--- src/main/kotlin/lesson_12/task_1.kt
+++ src/main/kotlin/lesson_12/task_1.kt
@@ -1,5 +1,33 @@
 package org.example.lesson_12
 
 fun main(){
+    val mondayWeather = WeatherData()
+    val sundayWeather = WeatherData()
+    mondayWeather.printWeatherData()
+    sundayWeather.printWeatherData()
+    with(mondayWeather){
+        dayTemp = 25
+        nightTemp = 9
+        isPrecipitation = true
+    }
+    with(sundayWeather){
+        dayTemp = 35
+        nightTemp = 19
+        isPrecipitation = false
+    }
+    mondayWeather.printWeatherData()
+    sundayWeather.printWeatherData()
+}
+
+class WeatherData{
+    var dayTemp = 20
+    var nightTemp = 10
+    var isPrecipitation = false
+
+    fun printWeatherData(){
+        println("Температура днём:$dayTemp. Температура ночью:$nightTemp. ${getPrecipitationAsString()}")
+    }
+
+    private fun getPrecipitationAsString():String = if (isPrecipitation) "Были осадки" else "Осадков не было"
 
 }
