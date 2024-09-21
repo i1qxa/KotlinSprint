2f36b2a5c299da1d698a6fcffab92b706cb8058c
Иван <cvb@sdf>
2024-09-21 11:19:34 +0300

    KS-15-2-weather-station Заменил if на when, добавил по 1 полю в классы

--- src/main/kotlin/lesson_15/task_2.kt
+++ src/main/kotlin/lesson_15/task_2.kt
@@ -2,22 +2,24 @@ package org.example.lesson_15
 
 fun main() {
     WeatherServer().apply {
-        sendStats(Temperature())
-        sendStats(PrecipitationAmount())
+        sendStats(Temperature(32))
+        sendStats(PrecipitationAmount(12))
     }
 }
 
 abstract class WeatherStationStats()
 
-class Temperature() : WeatherStationStats()
+class Temperature(val temperature: Int) : WeatherStationStats()
 
-class PrecipitationAmount() : WeatherStationStats()
+class PrecipitationAmount(val precipitationAmount: Int) : WeatherStationStats()
 
 class WeatherServer() {
 
     fun sendStats(data: WeatherStationStats) {
-        if (data is Temperature) println("Значение температуры успешно отправлено на сервер")
-        else println("Показатели осадков успешно отправлены на сервер")
+        when (data) {
+            is Temperature -> println("Значение температуры успешно отправлено на сервер")
+            is PrecipitationAmount -> println("Показатели осадков успешно отправлены на сервер")
+            else -> println("Ошибка, неизвестные показатели!")
+        }
     }
-
 }
