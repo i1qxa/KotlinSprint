85a0ffc7334416a80e97365bab407647fbcbd942
Кукса <kuiv1989@gmail.com>
2024-09-24 12:13:37 +0300

    KS-17-5-user Создал класс для работы с данными пользователя

---
+++ src/main/kotlin/lesson_17/task_5.kt
@@ -0,0 +1,29 @@
+package org.example.lesson_17
+
+fun main() {
+    User("Nick", "1234567").apply {
+        pass = "123"
+        println(pass)
+        login = "Admin"
+    }
+}
+
+class User(login:String, pass:String){
+
+    var pass = pass
+        get() {
+            var passHide = ""
+            repeat(field.length){
+                passHide+="*"
+            }
+            return passHide
+        }
+        set(value) = println("Вы не можете изменить пароль")
+
+    var login = login
+        set(value){
+            field = value
+            println("Логин успешно изменен")
+        }
+
+}
