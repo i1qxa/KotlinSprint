b918b4e759c08285411310b80ad25f356b7def7d
Иван <cvb@sdf>
2024-09-25 00:05:22 +0300

    KS-18-5-screen Создал класс для рисования фигур

---
+++ src/main/kotlin/lesson_18/task_5.kt
@@ -0,0 +1,21 @@
+package org.example.lesson_18
+
+abstract class Figure()
+
+class Dot() : Figure()
+
+class Circle() : Figure()
+
+class Rectangle() : Figure()
+
+class Screen() {
+
+    fun drawFigure(x:Int, y:Int, figure: Figure){
+
+    }
+
+    fun drawFigure(x:Float, y:Float, figure: Figure){
+
+    }
+
+}
