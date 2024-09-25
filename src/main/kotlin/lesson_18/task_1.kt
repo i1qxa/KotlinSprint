23a64240e8d85b929006c395b491c10d8cd80ed3
Иван <cvb@sdf>
2024-09-26 00:03:29 +0300

    KS-18-1-order Избавился от лишнего класса, перенес функции в методы класса

--- src/main/kotlin/lesson_18/task_1.kt
+++ src/main/kotlin/lesson_18/task_1.kt
@@ -3,7 +3,7 @@ package org.example.lesson_18
 fun main() {
 
     Order().apply {
-        printOrderInfo(1,"Milk")
+        printOrderInfo(1, "Milk")
     }
     Order().apply {
         printOrderInfo(2, listOf("Milk", "Avocado", "Bananas"))
@@ -11,15 +11,15 @@ fun main() {
 
 }
 
-class Order(){
+class Order() {
 
-    fun printOrderInfo(id: Int, goods:String) {
+    fun printOrderInfo(id: Int, goods: String) {
 
         println("Заказан товар:$goods")
 
     }
 
-    fun printOrderInfo(id: Int, goods:List<String>) {
+    fun printOrderInfo(id: Int, goods: List<String>) {
 
         println("Заказаны следующие товары:$goods")
 
