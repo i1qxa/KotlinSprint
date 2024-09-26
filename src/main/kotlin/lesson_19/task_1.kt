6d23bffaace19afe678907189cc9602ca3e510d2
Кукса <kuiv1989@gmail.com>
2024-09-26 10:11:10 +0300

    KS-19-1-aquarium Создал класс с перечислением всех доступных рыб

---
+++ src/main/kotlin/lesson_19/task_1.kt
@@ -0,0 +1,13 @@
+package org.example.lesson_19
+
+fun main() {
+    println("Вы можете добавить в свой аквариум следующие виды рыб")
+    println(FISH.entries)
+}
+
+enum class FISH{
+    GUPPY,
+    ANGELFISH,
+    GOLDFISH,
+    SIAMESE_FIGHTING_FISH
+}
