7c126c38e24e41236ebba44d968762a3229390e8
Кукса <kuiv1989@gmail.com>
2024-09-26 10:53:41 +0300

    KS-19-3-space-ship Создал класс космического корабля с различными ещё не реализованными методами

---
+++ src/main/kotlin/lesson_19/task_3.kt
@@ -0,0 +1,21 @@
+package org.example.lesson_19
+
+fun main() {
+    SpaceShip(1, "Сокол тысячелетия").landOn()
+}
+
+class SpaceShip(val id: Int, val name: String){
+
+    fun takeOff(){
+        //TODO нужно реализовать метод взлета корабля
+    }
+
+    fun landOn(){
+        TODO("Нужно реализовать метод посадки корабля")
+    }
+
+    fun shootingFromAnAsteroid(){
+        TODO("Нужно реализовать метод отстрела от астероида дополнив метод взлета")
+    }
+
+}
