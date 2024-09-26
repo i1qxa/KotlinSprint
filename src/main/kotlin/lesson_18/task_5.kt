60c422f67dc182db9d713167c20cc21622327de5
Иван <cvb@sdf>
2024-09-26 23:04:24 +0300

    KS-18-5-screen В методах отрисовки добавил методы для каждого типа фигур

--- src/main/kotlin/lesson_18/task_5.kt
+++ src/main/kotlin/lesson_18/task_5.kt
@@ -11,11 +11,37 @@ class Rectangle() : Figure()
 class Screen() {
 
     fun drawFigure(x: Int, y: Int, figure: Figure) {
-
+        when(figure){
+            is Dot ->{
+                println("Точка. Координаты - целочисленные")
+            }
+            is Circle ->{
+                println("Круг. Координаты - целочисленные")
+            }
+            is Rectangle ->{
+                println("Прямоугольник. Координаты - целочисленные")
+            }
+            else ->{
+                println("Ошибка. Неизвестная фигура")
+            }
+        }
     }
 
     fun drawFigure(x: Float, y: Float, figure: Figure) {
-
+        when(figure){
+            is Dot ->{
+                println("Точка. Координаты - с плавающей точкой")
+            }
+            is Circle ->{
+                println("Круг. Координаты - с плавающей точкой")
+            }
+            is Rectangle ->{
+                println("Прямоугольник. Координаты - с плавающей точкой")
+            }
+            else ->{
+                println("Ошибка. Неизвестная фигура")
+            }
+        }
     }
 
 }
