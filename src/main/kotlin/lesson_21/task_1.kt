f268ae7c2b402fa7f1ab54524d7e125475b0868a
Иван <cvb@sdf>
2024-09-28 22:43:51 +0300

    KS-21-1-string-vowel Сделал подсчет гласных без регулярки

--- src/main/kotlin/lesson_21/task_1.kt
+++ src/main/kotlin/lesson_21/task_1.kt
@@ -1,12 +1,10 @@
 package org.example.lesson_21
 
-import java.util.*
-
 fun main() {
     println("hello".vowelCount())
 }
 
 fun String.vowelCount(): Int {
-    val vowelRegex = "[aeiouy]".toRegex()
-    return this.count { vowelRegex.containsMatchIn(it.toString().lowercase(Locale.getDefault())) }
+    val vowels = "aeiouy"
+    return this.count { it in vowels }
 }
