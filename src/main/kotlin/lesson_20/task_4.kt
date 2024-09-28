d20a885784d944ce1fce71fe73d2bbfff41595b1
Иван <cvb@sdf>
2024-09-28 22:34:25 +0300

    KS-20-4-list Переделал программу так чтобы итоговый список имел тип элементов Unit

--- src/main/kotlin/lesson_20/task_4.kt
+++ src/main/kotlin/lesson_20/task_4.kt
@@ -3,11 +3,7 @@ package org.example.lesson_20
 fun main() {
 
     val listOfButtons = mutableListOf<String>("Help", "Submit", "Apply", "Cancel", "Abort", "Finish", "Begin")
-    listOfButtons.map {
-        if (listOfButtons.indexOf(it) % 2 == 0) {
-            { println("Нажат элемент: $it") }()
-        } else {
-            it
-        }
+    listOfButtons.filter { listOfButtons.indexOf(it) % 2 != 0 }.map {
+        println("Нажат элемент: $it")
     }
 }
